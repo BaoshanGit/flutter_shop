b291b17b255434befcc5731d9b4205b101d38e59
baoshan <fighter_obj@163.com>
2020-07-01 18:30:54 +0800

    2020-07-01:轮播图制作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'widgets/swiper_widget.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String param;
-  @override
-  void initState() {
-    getHomePageContent().then((val){
-      setState(() {
-        param = val;
-      });
-    });
-    super.initState();
-  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("首页")),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context , val){
+          if(val.hasData){
+            var data = json.decode(val.data.toString());
+            List<Map> swiperDataList = (data["data"]["slides"] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiperDataList)
+              ],
+            );
+          }else{
+            return Center(
+              child: Text("加载中...."),
+            );
+          }
+        },
+      ),
+    );
   }
 }
---
+++ lib/pages/widgets/swiper_widget.dart
@@ -0,0 +1,27 @@
+/*
+  轮播图组件
+ */
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+class SwiperDiy extends StatelessWidget {
+  //接收轮播图片地址
+  final List swiperDataList;
+  SwiperDiy({Key key , this.swiperDataList}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //设置轮播组件容器高度
+      height: 333.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context , int index){
+          return Image.network(swiperDataList[index]["image"],fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        autoplay: true,//是否自动播放
+        pagination: new SwiperPagination(),//?
+      ),
+    );
+  }
+}
+
